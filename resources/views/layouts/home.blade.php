852f4b6f00c882ffe9d715d2b10b527f3e85d918
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2020-05-02 10:47:48 +0700

    Update : chacebreaker head

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -11,22 +11,22 @@
         Kelas Bisa
     </title>
 
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ url('apple-icon-57x57.png') }}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ url('apple-icon-60x60.png') }}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ url('apple-icon-72x72.png') }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ url('apple-icon-76x76.png') }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ url('apple-icon-114x114.png') }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ url('apple-icon-120x120.png') }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ url('apple-icon-144x144.png') }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ url('apple-icon-152x152.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('apple-icon-180x180.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ url('android-icon-192x192.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ url('favicon-96x96.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ url('apple-icon-57x57.png?') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ url('apple-icon-60x60.png?') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ url('apple-icon-72x72.png?') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ url('apple-icon-76x76.png?') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ url('apple-icon-114x114.png?') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ url('apple-icon-120x120.png?') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ url('apple-icon-144x144.png?') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ url('apple-icon-152x152.png?') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('apple-icon-180x180.png?') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ url('android-icon-192x192.png?') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('favicon-32x32.png?') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ url('favicon-96x96.png?') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicon-16x16.png?') }}">
     <link rel="manifest" href="{{ url('manifest.json')}}">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="{{ url('ms-icon-144x144.png') }}">
+    <meta name="msapplication-TileImage" content="{{ url('ms-icon-144x144.png?') }}">
     <meta name="theme-color" content="#ffffff">
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
