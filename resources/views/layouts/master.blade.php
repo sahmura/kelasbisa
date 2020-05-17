1a2c9eb82a977244baf6e504402a69c1f04cea50
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2020-05-17 11:45:39 +0700

    Update : update service worker path

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -88,7 +88,8 @@
     <script>
         if ('serviceWorker' in navigator && 'PushManager' in window) {
             window.addEventListener('load', function () {
-                navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+                navigator.serviceWorker.register("{{ url('/service-worker.js') }}").then(function (
+                    registration) {
                     // Registration was successful
                     console.log('ServiceWorker registration successful with scope: ', registration
                         .scope);
